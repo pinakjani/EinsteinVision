a2d5ff067105172d45d3b4658e64991087e00326
Aniket Patil <aniketp1597@gmail.com>
2023-06-20 19:57:07 -0400

    clean up code

--- data/generate_frames_tesla.py
+++ data/generate_frames_tesla.py
@@ -1,14 +1,6 @@
 import os
 import cv2
 
-def create_directory_at(path, dir):
-    dir_path = os.path.join(path, dir)
-    try:
-        os.mkdir(dir_path)
-    except OSError:
-        print ('Error: Directory may already exist or path is invalid')
-    return dir_path
-
 def extract_frames_from_video(path_to_video):
     video = cv2.VideoCapture(path_to_video)
     current_frame = 0
@@ -35,8 +27,9 @@ def extract_frames_from_video(path_to_video):
 def extract_data(input_dir, output_dir, view_points):
     input_path = os.path.join(os.getcwd(), input_dir)
     output_path = os.getcwd()
-    cwd = create_directory_at(output_path, output_dir)
-    for subdir, dirs, files in os.walk(input_path):
+    cwd = os.path.join(output_path, output_dir)
+    os.makedirs(cwd, exist_ok=True)
+    for subdir, _, files in os.walk(input_path):
         for file in files:
             data_dir_list = subdir.split('/')
             if (data_dir_list[-3] == "Sequences" and data_dir_list[-1] == "Undist"):
@@ -44,19 +37,19 @@ def extract_data(input_dir, output_dir, view_points):
                 keyword = file_split[-1].split('_')[0]  ## check the viewpoint direction
                 for view_point in view_points:
                     if (keyword == view_point):
-                        dir_name = data_dir_list[-2]   
-                        path = create_directory_at(cwd, dir_name)
-                        path = create_directory_at(path, keyword)
+                        scene_name = data_dir_list[-2]
+                        path = os.path.join(cwd, scene_name, keyword)
+                        os.makedirs(path, exist_ok=True)
                         os.chdir(path)
-                        print("Extracting data in {}".format(path))
+                        print("Extracting {} data of {} view".format(scene_name, keyword))
                         path_to_video = os.path.join(subdir, file)
                         extract_frames_from_video(path_to_video)
                     
 
 def main():
-    input_dir = 'tesla_raw'                          ## Input directory
-    output_dir = 'tesla_proc'                       ## Name of the output directory
-    view_points = ["front"]                         ## List of view points
+    input_dir = 'tesla_raw'             ## Input directory
+    output_dir = 'tesla_proc'           ## Name of the output directory
+    view_points = ["front"]             ## List of view points
 
     extract_data(input_dir, output_dir, view_points)
 
